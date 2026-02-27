ca1a8665859ae30c373e6dc98a2ce105c8803bf9
Sandeep Kumar <sandeepkumar073113@gmail.com>
2026-02-27 23:08:35 +0530

    Add : Adding Environment

---
+++ src/environment/environment.dev.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA_PyHLIw2LaNceVe-TrSgoa5vMqJI2s4g",
+    authDomain: "news-aggregator-83020.firebaseapp.com",
+    projectId: "news-aggregator-83020",
+    storageBucket: "news-aggregator-83020.firebasestorage.app",
+    messagingSenderId: "266370758403",
+    appId: "1:266370758403:web:10e33717b7d54287551b36",
+    measurementId: "G-WHFXS46N2Y"
+  }
+};
